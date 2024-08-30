faaf199f16110ee2c470337fe5bee4b8cc06b833
Taha-Shaban-Kaamel <tahashaban743@gmail.com>
2024-08-30 12:02:35 +0300

    add new projects and updete resume

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -27,7 +27,7 @@ window.onscroll = () => {
   
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-      console.log(entry);
+      
       
       if (entry.isIntersecting) {
         entry.target.classList.add('show');
